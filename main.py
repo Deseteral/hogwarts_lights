a3da7091225b92f32cd03aa159ba3739c49bf3af
Deseteral <deseteral@gmail.com>
2021-02-07 21:14:46 +0100

    Stuff

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ pwm_r = machine.PWM(machine.Pin(0), 5000)
 pwm_g = machine.PWM(machine.Pin(4), 5000)
 pwm_b = machine.PWM(machine.Pin(5), 5000)
 
+is_on=False
+r_value = 1023
+g_value = 1023
+b_value = 1023
+
 def light_on():
-    pwm_r.duty(1023)
-    pwm_g.duty(1023)
-    pwm_b.duty(1023)
+    is_on = True
+    apply_lights_value()
 
 def light_off():
+    is_on = False
     pwm_r.duty(0)
     pwm_g.duty(0)
     pwm_b.duty(0)
+
+def apply_lights_value():
+    if not is_on:
+        return
+    pwm_r.duty(r_value)
+    pwm_g.duty(g_value)
+    pwm_b.duty(b_value)
 
 light_off()
 
@@ -35,6 +47,12 @@ def homepage(request, response):
     return
 
 @app.route('/light/off', methods=['POST'])
+def homepage(request, response):
+    light_off()
+    yield from picoweb.jsonify(response, {'light': 'off'})
+    return
+
+@app.route('/light/values', methods=['POST'])
 def homepage(request, response):
     light_off()
     yield from picoweb.jsonify(response, {'light': 'off'})
